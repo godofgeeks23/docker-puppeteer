dd9e7f06185b99188da952b75df9d3f9b1ab7de2
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-04-20 22:33:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ import puppeteer from 'puppeteer';
 (async () => {
     // Launch the browser and open a new blank page
     const browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      });
+        executablePath: '/usr/bin/google-chrome',
+        args: [
+            '--disable-setuid-sandbox',
+            '--no-sandbox'
+        ]
+    });
     const page = await browser.newPage();
 
     // Navigate the page to a URL
