784e6d3fd140fa0fcf4bb3dfb6e6350e1c37b4df
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-04-23 17:58:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,18 +8,21 @@ import puppeteer from 'puppeteer';
             '--no-sandbox'
         ]
     });
+
     const page = await browser.newPage();
     await page.goto('https://developer.chrome.com/');
     await page.setViewport({ width: 1080, height: 1024 });
     await page.type('.devsite-search-field', 'automate beyond recorder');
 
     const searchResultSelector = '.devsite-result-item-link';
+
     await page.waitForSelector(searchResultSelector);
     await page.click(searchResultSelector);
 
     const textSelector = await page.waitForSelector(
         'text/Customize and automate'
     );
+    
     const fullTitle = await textSelector?.evaluate(el => el.textContent);
 
     console.log('The title of this blog post is "%s".', fullTitle);
