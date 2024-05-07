45563ed882e5f46e63b8e149ead5234b85512708
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-05-07 23:20:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import puppeteer from 'puppeteer';
 
 (async () => {
-    // create a new browser instance
     const browser = await puppeteer.launch({
         executablePath: '/usr/bin/google-chrome',
         args: [
@@ -10,13 +9,11 @@ import puppeteer from 'puppeteer';
         ]
     });
 
-    // create a new page instance
     const page = await browser.newPage();
     await page.goto('https://developer.chrome.com/');
     await page.setViewport({ width: 1080, height: 1024 });
     await page.type('.devsite-search-field', 'automate beyond recorder');
 
-    // click on the search button
     const searchResultSelector = '.devsite-result-item-link';
 
     await page.waitForSelector(searchResultSelector);
