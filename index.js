542dc854759b2bc02b2be951d466d6d28d9afedf
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-10-25 09:44:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
-import puppeteer from 'puppeteer';
+import puppeteer from "puppeteer";
 
 (async () => {
-    const browser = await puppeteer.launch({
-        executablePath: '/usr/bin/google-chrome',
-        args: [
-            '--disable-setuid-sandbox',
-            '--no-sandbox'
-        ]
-    });
+  const browser = await puppeteer.launch({
+    executablePath: "/usr/bin/google-chrome",
+    args: ["--disable-setuid-sandbox", "--no-sandbox"],
+  });
 
-    const page = await browser.newPage();
-    await page.goto('https://developer.chrome.com/');
-    await page.setViewport({ width: 1080, height: 1024 });
-    await page.type('.devsite-search-field', 'automate beyond recorder');
+  const page = await browser.newPage();
+  await page.goto("https://developer.chrome.com/");
+  await page.setViewport({ width: 1080, height: 1024 });
+  await page.type(".devsite-search-field", "automate beyond recorder");
 
-    const searchResultSelector = '.devsite-result-item-link';
+  const searchResultSelector = ".devsite-result-item-link";
 
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
+  await page.waitForSelector(searchResultSelector);
+  await page.click(searchResultSelector);
 
-    const textSelector = await page.waitForSelector(
-        'text/Customize and automate'
-    );
+  const textSelector = await page.waitForSelector(
+    "text/Customize and automate"
+  );
 
-    const fullTitle = await textSelector?.evaluate(el => el.textContent);
+  const fullTitle = await textSelector?.evaluate((el) => el.textContent);
 
-    console.log('Blog post title - "%s".', fullTitle);
+  console.log('Blog post title - "%s".', fullTitle);
 
-    await browser.close();
-
+  await browser.close();
 })();
